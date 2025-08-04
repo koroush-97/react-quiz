bd8bcd514eabf15a85abede99a50ac8da6222138
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-04 20:16:02 +0330

    use Main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Header from "./Header";
+import Main from "./components/Main";
 export default function App() {
   return (
     <div className="app">
       <Header />
-      <main className="main">
+      <Main>
         <p>1/15</p>
         <p>Questions</p>
-      </main>
+      </Main>
     </div>
   );
 }
