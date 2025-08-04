f8328c392e13f3acde8a3e8c82bc29a65cbea9ce
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-04 20:57:13 +0330

    set inital state and reducer func

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./components/Main";
+
+function reducer(stae, action) {}
+const initalState = {
+  questions: [],
+};
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initalState);
+
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
