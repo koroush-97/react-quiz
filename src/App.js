87fe4989505f07abd800f0fa129f536d6e916ce9
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-04 21:07:32 +0330

    set condition for get api in reducer

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./components/Main";
+import { type } from "@testing-library/user-event/dist/type";
 
-function reducer(stae, action) {}
 const initalState = {
   questions: [],
+  status: "loading",
 };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    default:
+      throw new Error(" Action unkonwn ");
+  }
+}
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initalState);
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => console.log("Error"));
   }, []);
 
