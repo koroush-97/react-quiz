eff29dcb3608717ac32f3b5c63d7f46e56c775d5
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-04 20:51:51 +0330

    set featch to get dummy api for questions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./components/Main";
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log("Error"));
+  }, []);
+
   return (
     <div className="app">
       <Header />
