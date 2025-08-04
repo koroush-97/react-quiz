bd492a7cdefaa2feeb7db9b03f55a70c390dbee7
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-04 16:12:40 +0330

    delete all unused commit

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -6,10 +6,10 @@ function reducer(state, action) {
 
   switch (action.type) {
     case "dec":
-      return { ...state, count: state.count - 1 };
+      return { ...state, count: state.count - state.step };
 
     case "inc":
-      return { ...state, count: state.count + 1 };
+      return { ...state, count: state.count + state.step };
 
     case "setCount":
       return { ...state, count: action.payload };
@@ -23,12 +23,9 @@ function reducer(state, action) {
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-
   const initalState = { count: 0, steo: 1 };
   const [state, dispatch] = useReducer(reducer, initalState);
   const { count, step } = state;
-  // const [step, setStep] = useState(1);
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -36,31 +33,21 @@ function DateCounter() {
 
   const dec = function () {
     dispatch({ type: "dec" });
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
   };
 
   const inc = function () {
     dispatch({ type: "inc" });
-
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
     dispatch({ type: "setCount", payload: Number(e.target.value) });
-    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
     dispatch({ type: "setStep", payload: Number(e.target.value) });
-    // setStep(Number(e.target.value));
   };
 
-  const reset = function () {
-    // setCount(0);
-    // setStep(1);
-  };
+  const reset = function () {};
 
   return (
     <div className="counter">
