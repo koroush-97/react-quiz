d3831940eb2467d03d385f59223232bc0c147333
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-05 12:48:38 +0330

    pass dynamic number of questions to startScreen

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -2,7 +2,7 @@ export default function StartScreen({ numQuestions }) {
   return (
     <div className="start">
       <h2>WelCome to The React Quiz!</h2>
-      <h3>X question to test your React mastery</h3>
+      <h3>{numQuestions} question to test your React mastery</h3>
       <button> Let's Start </button>
     </div>
   );
