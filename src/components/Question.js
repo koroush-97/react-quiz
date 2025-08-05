28b1ddc169932185fcc015ce5164c32182452aab
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-05 16:12:44 +0330

    match class with correct answer

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,7 +6,7 @@ export default function Question({ question, answer, dispatch }) {
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} />
+      <Options question={question} dispatch={dispatch} answer={answer} />
     </div>
   );
 }
