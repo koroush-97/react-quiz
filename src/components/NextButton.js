4467feb1db9a9a79dc67d005ae73fd68f78d4fbb
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-05 22:50:10 +0330

    fix bug of don't show button

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -10,4 +10,13 @@ export default function NextButton({ dispatch, answer, index, numQuestions }) {
         Next
       </button>
     );
+  if (index === numQuestions - 1)
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
 }
