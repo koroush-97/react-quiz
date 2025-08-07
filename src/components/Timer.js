de979a63232ad7085a0ae300ed000d888e4f90f6
koroush bagheri <kooroshbagheri97director@gmail.com>
2025-08-07 12:34:39 +0330

    set test code for timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,3 +1,11 @@
+import { useEffect } from "react";
+
 export default function Timer() {
+  useEffect(function () {
+    setInterval(function () {
+      console.log("tick");
+    }, 1000);
+  }, []);
+
   return <div className="timer">05:00</div>;
 }
